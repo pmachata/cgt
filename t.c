4dee4ab4e2c5401ba2bf6d79b16d7806e0b27ed0
Petr Machata <pmachata@gmail.com>
2018-12-28 00:03:18 +0100

    Add t.c
    Some testcases found when building elfutils.

---
+++ t.c
@@ -0,0 +1,19 @@
+static inline void
+target_expr (int width)
+{
+    ({
+        width;
+        width;
+    });
+}
+
+static inline void
+asm_expr (void)
+{
+    const unsigned char *Addr;
+    unsigned short int __v, __x = (unsigned short int) (*Addr);
+    __asm__ ("rorw $8, %w0"
+             : "=r" (__v)
+             : "0" (__x)
+             : "cc");
+}
